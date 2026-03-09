022dcb811d761756d146929a872c09bc0bb3ae5f
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-03-09 17:00:50 +0800

    Country postal code validation

--- script.js
+++ script.js
@@ -7,18 +7,24 @@ email.addEventListener("input", (event) => {
         emailError.textContent = "";
         emailError.className = "error"; 
     } else {
-        showError();
+        showEmailError();
     }
 });
 
 form.addEventListener("submit", (event) => {
     if (!email.validity.valid) {
-        showError();
+        showEmailError();
+        event.preventDefault();
+    };
+    checkPostalCode();
+    if (!postalCodeField.validity.valid) {
         event.preventDefault();
     }
+
+
 });
 
-function showError() {
+function showEmailError() {
     if (email.validity.valueMissing) {
         emailError.textContent = "You need to enter an email address.";
     } else if (email.validity.typeMismatch) {
@@ -28,3 +34,55 @@ function showError() {
     }
     emailError.className = "error active";
 }
+
+const countrySelect = document.getElementById("country");
+const postalCodeField = document.getElementById("postal-code");
+const postalCodeError = document.querySelector("#postal-code + span.error");
+
+function checkPostalCode() {
+    const constraints = {
+        ch: [
+            "^(CH-)?\\d{4}$",
+            "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+        ],
+        fr: [
+            "^(F-)?\\d{5}$",
+            "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+        ],
+        de: [
+            "^(D-)?\\d{5}$",
+            "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+        ],
+        nl: [
+            "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+            "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+        ],
+    };
+
+    const country = countrySelect.value;
+
+    const constraint = new RegExp(constraints[country][0], "");
+    console.log(constraint);
+
+    if (constraint.test(postalCodeField.value)) {
+        postalCodeField.setCustomValidity("");
+        postalCodeError.textContent = "";
+        postalCodeError.className = "error"; 
+    } else {
+        const errorMsg = constraints[country][1];
+        postalCodeField.setCustomValidity(errorMsg);
+        showPostalCodeError(errorMsg)
+    }
+}
+
+function showPostalCodeError(message) {
+    if (postalCodeField.validity.valueMissing) {
+        postalCodeError.textContent = "You need to enter postal code.";
+    } else if (postalCodeField.validity.customError) {
+        postalCodeError.textContent = message;
+    } 
+    postalCodeError.className = "error active";
+}
+
+countrySelect.addEventListener("change", checkPostalCode);
+postalCodeField.addEventListener("input", checkPostalCode);
